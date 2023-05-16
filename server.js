a817df8b333a32a183bf7e58115c2883ab531b47
Tjcarroll1 <tjcarroll1@icloud.com>
2023-05-16 21:24:23 +1000

    added connections and seeds data for tour-seed.js

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
-//test
+const express = require('express');
+const routes = require('./routes');
+// import sequelize connection
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// check this setup of handlebars is correct
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+
+
+app.use(routes);
+
+// sync sequelize models to the database, then turn on the server
+sequelize.sync({ force: false }).then(() => {
+app.listen(PORT, () => {console.log(`App listening on port ${PORT}!`);
+});
+});
+
+
