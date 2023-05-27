f5a82a92e864bd9c9197a243e746264039d4eedc
Sawako Goshima <sawako@192-168-1-103.tpgi.com.au>
2023-05-27 16:10:25 +1000

    Updated readme file

--- server.js
+++ server.js
@@ -41,6 +41,8 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/tour', express.static('public'));
+app.use('/tours', express.static('public'));
 
 
 app.use(routes);
